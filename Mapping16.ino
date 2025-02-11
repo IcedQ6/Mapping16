4995983e3b1f0a694135c43b75af79a2f8467d36
IcedQ6 <burningbabystudios@gmail.com>
2025-02-11 17:49:38 -0500

    Comments describing how the game will work

--- Mapping16.ino
+++ Mapping16.ino
@@ -12,7 +12,30 @@ cover the device with their hand, or shout into it depending on different Neopix
 Upon taking too long or making a mistake, the game will end and your score will be displayed.
 
 TO PLAY:
-Hold the 
+Hold the device with one hand parralel to the ground and press the left button to start the game.
+
+2-3 Neopixels will light in the direction you need to move the sensor,
+so hold it firm when you move your arm in that direction.
+If all the neopixels start flashing, you need to shout into the device.
+If the neopixels start to alternate a dim glow, you need to cover the device with your other hand.
+
+If you succeeded, a short animation will play along with a possitive beep.
+If you did the wrong action or took too long, 
+the neopixels will flash red with a long buzzer and the game ends. Your score is then displayed.
+
+Your score is based on the number of LEDs that light up, ranging as so:
+  0-5: RED
+  6-10: ORANGE
+  11-15: YELLOW
+  16-20: GREEN
+  21-25: BLUE
+  26-30: INDIGO
+  31-35: VIOLET
+  36-40: WHITE
+  41-45: RAINBOW
+  (45 is the highest score)
+
+To play again, just press the left button again. Scores are not tracked.
 */
 
 void setup() {
